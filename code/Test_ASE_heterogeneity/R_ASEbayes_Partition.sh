124a2abbe5980898a68d37a7bb8a7cebb4b6ca66
katyamack-hub <62080589+katyamack-hub@users.noreply.github.com>
2022-09-22 15:40:09 -0700

    Create R_ASEbayes_Partition.sh

---
+++ code/Test_ASE_heterogeneity/R_ASEbayes_Partition.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+##Implements method in Pirinen et al. 2014 to partition ASE genes into groups
+##Requires code from Pirinen et al. 2014: https://www.mv.helsinki.fi/home/mjxpirin/download.html
+
+echo "source(\"stickleback_bayestest/ASE_29Dec2014.R\")
+df <- data.frame(matrix(ncol = 6, nrow = 0))
+##datafile, low tissues removed and set to zero
+j <- read.csv(\"all_tissues_merged.calledin3_5depth.2.repssummed.withzeros.whead.onlytime2.rmzero.txt\",header=FALSE,sep=\" \")
+
+pr.beta=c(2000,2000,80,36,80,7)
+" > ${list}_run.R
+
+#List of genes to be analyzed
+cat ${list} | while read gene
+do
+echo "
+g <- j[j\$V1 == '${gene}',]
+y=matrix(as.double(g[,c(2:15)]),ncol=2,byrow=TRUE)
+res_${gene}=gtm(y=y,pr.beta,niter=2000,burnin=10,two.sided=TRUE,independent=TRUE,model.strong.ase=TRUE,pr.dist=NULL)
+t2<-t(res_${gene}\$top.het.model)
+
+write.csv(res_${gene}\$state.posteriors,\"TIME2_only/${gene}.statepos.out\")
+write.csv(res_${gene}\$indiv.posteriors,\"TIME2_only/${gene}.ind.postiors.out\")
+write.csv(t2,\"TIME2_only/${gene}.hetmodels.out\")
+" >> ${list}_run.R
+done
+
+
+R CMD BATCH ${list}_run.R
